f73233c480338db57bec3e4efebf431e20b7849e
Leon Grin <41086608+leongrin@users.noreply.github.com>
2021-11-24 09:13:33 -0300

    updates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 import {Router} from '@angular/router';
 import {AuthStoreService} from '../services/auth-store.service';
+import {User} from '../model/user';
 
 @Component({
   selector: 'login',
@@ -33,8 +34,13 @@ export class LoginComponent implements OnInit {
   login() {
     const val = this.form.value;
     this.authStoreServ.login(val.email, val.password)
-      .subscribe(() => {
-        this.router.navigateByUrl('/courses');
+      .subscribe((user: User) => {
+        console.log(`data => ${Object.keys(user)} and ${Object.values(user)}`);
+        if (!user.email) {
+          console.log('Not a user. We got an error back.');
+        } else {
+          this.router.navigateByUrl('/courses');
+        }
       }, err => alert('Login failed'));
   }
 
