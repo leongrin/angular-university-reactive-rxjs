4855838104876ab4e59e1c23d6a897c9d316dda0
Leon Grin <41086608+leongrin@users.noreply.github.com>
2021-12-13 13:55:11 -0300

    updates

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/router';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css']
